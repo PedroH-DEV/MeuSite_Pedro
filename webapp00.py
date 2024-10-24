67b782e757a895de462351ead05178cda71ae7d7
Pedro Henrique <phenrique3721@gmail.com>
2024-10-23 21:57:05 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -2,13 +2,13 @@
 import streamlit as st
   
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("SITE TESTE")
+st.title("SANTA ANGELA - LUGAR DE MORAR BEM https://www.google.com/url?sa=i&url=https%3A%2F%2Fsantaangelaconstrutora.com.br%2F&psig=AOvVaw0VMRRZON2UFannBadyRNT9&ust=1729817734562000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCNCHt7HnpYkDFQAAAAAdAAAAABAG")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header("TESTE DE CABEÇALHO")
+st.header("Essa construtora é D+")
 
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("Sub Cabeçalho")
+st.subheader("Notícia: A melhor construtira da América Latina")
 
 # Use st.write("") para adicionar um texto ao seu Web app
-st.write("LERO-LERO")
+st.write("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRTTIDwib3TUNXUOFc6gCmXQJwtbqFdtNpJpsllc96pVLRvIaGzw_Q8wG-RuvEUqniSoo&usqp=CAU")
