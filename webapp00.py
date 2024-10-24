a4c966ca2d32b59c7f4831a983448c0d8e56284a
Pedro Henrique <phenrique3721@gmail.com>
2024-10-23 22:00:51 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -1,28 +1,24 @@
-# MEU PRIMEIRO WEB APP
 import streamlit as st
-  
-# Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("SANTA ANGELA - LUGAR DE MORAR BEM")
-
-import requests
 from PIL import Image
+import requests
 from io import BytesIO
 
-# URL da imagem
+# Carregando a imagem a partir da URL
 url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRTTIDwib3TUNXUOFc6gCmXQJwtbqFdtNpJpsllc96pVLRvIaGzw_Q8wG-RuvEUqniSoo&usqp=CAU"
-
-# Fazendo a requisição da imagem
 response = requests.get(url)
 img = Image.open(BytesIO(response.content))
 
-# Exibindo a imagem
-img.show()
+# Título
+st.title("SANTA ANGELA - LUGAR DE MORAR BEM")
 
-# Use st.header("") para adicionar um CABEÇALHO ao seu Web app
+# Cabeçalho
 st.header("Essa construtora é D+")
 
-# Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("Notícia: A melhor construtira da América Latina")
+# Subcabeçalho
+st.subheader("Notícia: A melhor construtora da América Latina")
 
-# Use st.write("") para adicionar um texto ao seu Web app
+# Adicionando a imagem ao app
+st.image(img, caption='Santa Angela - Excelência em Construção', use_column_width=True)
+
+# Texto
 st.write("Empresa de Sucesso")
