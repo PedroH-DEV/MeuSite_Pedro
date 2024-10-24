7555482b5212d2f0e1b0262e99134b59775dc939
Pedro Henrique <phenrique3721@gmail.com>
2024-10-23 22:04:45 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -3,10 +3,15 @@ from PIL import Image
 import requests
 from io import BytesIO
 
-# Carregando a imagem a partir da URL
-url = "https://cdn-app-privally-io.s3.amazonaws.com/env/suite/images/context/asset/centrals/0003/00002667/darkLogo/20211228211245.png"
+# Carregando a imagem principal a partir da URL
+url = "https://santaangelaconstrutora.com.br/wp-content/uploads/2021/07/Implantacao-JB.jpg"
 response = requests.get(url)
 img = Image.open(BytesIO(response.content))
+
+# Carregando um GIF animado
+gif_url = "https://media.giphy.com/media/J1kHMf6nUktpPsaK9j/giphy.gif"
+gif_response = requests.get(gif_url)
+gif_bytes = BytesIO(gif_response.content)
 
 # Título
 st.title("SANTA ANGELA - LUGAR DE MORAR BEM")
@@ -17,8 +22,11 @@ st.header("Essa construtora é D+")
 # Subcabeçalho
 st.subheader("Notícia: A melhor construtora da América Latina")
 
-# Adicionando a imagem ao app
+# Adicionando a imagem principal ao app
 st.image(img, caption='Santa Angela - Excelência em Construção', use_column_width=True)
 
-# Texto
+# Exibindo um GIF animado
+st.image(gif_bytes, caption='A evolução nunca para!', use_column_width=True)
+
+# Texto adicional
 st.write("Empresa de Sucesso")
